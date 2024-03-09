5e4c40c46962bbf0c64aa09f210aea854b691fc0
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-09 22:45:01 +0530

    Home page form implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import AppButton from "@/components/common/AppButton";
 import Image from "next/image";
+import { FormEvent } from "react";
 
 export default function Home() {
-    return <>Hi Nishant</>;
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+
+        console.log("formData", formData);
+    };
+
+    return (
+        <div className="flex items-center justify-center ">
+            <form onSubmit={handleSubmit} className="w-[50%]  text-center">
+                <input
+                    type="text"
+                    placeholder="What's your plan?"
+                    name="plan"
+                    className="input input-bordered w-full max-w-md text-sm my-2"
+                />
+
+                <textarea
+                    placeholder="Can you describe it briefly?"
+                    name="planDetails"
+                    className="textarea textarea-bordered textarea-sm w-full max-w-md"
+                ></textarea>
+
+                <AppButton btnText={"Submit"} />
+            </form>
+        </div>
+    );
 }
