2325b9ab64e3a2b3648f7ef5c54c4bfdbf601a6d
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-10 07:06:54 +0530

    Fixes

--- src/app/api/formsubmit/route.ts
+++ src/app/api/formsubmit/route.ts
@@ -44,17 +44,12 @@ export async function GET(request: Request, response: Response) {
 }
 
 export async function DELETE(request: NextApiRequest, response: Response) {
-    // const body = await request.text();
     const id = request.query.id;
 
-    // console.log("Delete Body", _id);
-    // const { _id } = JSON.parse(body);
     console.log("Delete id", id);
     try {
         await connectToDb();
-        const deletePlanById = await Plan.findByIdAndDelete(
-            "65ecf8a976e03fa7963c1f44"
-        );
+        const deletePlanById = await Plan.findByIdAndDelete(id);
 
         console.log("DEletePlanbyId", deletePlanById);
         return NextResponse.json(deletePlanById);
@@ -67,86 +62,3 @@ export async function DELETE(request: NextApiRequest, response: Response) {
         });
     }
 }
-
-// export async function GET(request: Request, response: Response) {
-//     try {
-//         await connectToDb();
-//         const allPlans = await Plan.find();
-
-//         // console.log("All Plans", allPlans);
-
-//         return NextResponse.json(allPlans);
-//     } catch (error) {
-//         return NextResponse.json({
-//             status: 500,
-//             message: "Failed to retrieve plan details. Please try again later.",
-//         });
-//     }
-// }
-
-// async function getAllResources() {
-//     try {
-//         await connectToDb();
-//         const allPlans = await Plan.find();
-
-//         // console.log("All Plans", allPlans);
-
-//         return NextResponse.json(allPlans);
-//     } catch (error) {
-//         return NextResponse.json({
-//             status: 500,
-//             message: "Failed to retrieve plan details. Please try again later.",
-//         });
-//     }
-// }
-
-// type IdType = string | string[] | undefined;
-// async function deleteResource(_id: IdType) {
-//     // const body = await request.text();
-
-//     console.log("Delete Body");
-//     // const { _id } = JSON.parse(body);
-//     console.log("Delete id", _id);
-//     try {
-//         await connectToDb();
-//         const deletePlanById = await Plan.findByIdAndDelete(_id);
-
-//         console.log("DEletePlanbyId", deletePlanById);
-//         return NextResponse.json(deletePlanById);
-//     } catch (error) {
-//         return NextResponse.json({
-//             status: 500,
-//             message:
-//                 "Failed to retrieve delete plan details. Please try again later.",
-//         });
-//     }
-// }
-
-// export default async function handler(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ) {
-//     const { method, query, body } = req;
-
-//     switch (method) {
-//         case "GET":
-//             const resources = await getAllResources();
-//             res.status(200).json(resources);
-//             break;
-//         // case 'POST':
-//         //     const newResource = await createResource(body);
-//         //     res.status(201).json(newResource);
-//         //     break;
-//         // case 'PUT':
-//         //     const updatedResource = await updateResource(query.id, body);
-//         //     res.status(200).json(updatedResource);
-//         //     break;
-//         case "DELETE":
-//             const deletedResource = await deleteResource(query?.id);
-//             res.status(200).json(deletedResource);
-//             break;
-//         default:
-//             res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
-//             res.status(405).end(`Method ${method} Not Allowed`);
-//     }
-// }
